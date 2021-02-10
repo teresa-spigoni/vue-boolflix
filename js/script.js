19ca8a5c6665b65aa5cad377baaab9cac8bf9a4a
Teresa Spigoni <teresaspigoni@gmail.com>
2021-02-10 12:14:41 +0100

    milestone 2 stars and tv series

--- js/script.js
+++ js/script.js
@@ -1,22 +1,70 @@
 new Vue({
     el: '#app',
     data: {
-        searchedMovie: '',
-        moviesList: []
+        searched: '',
+        moviesList: [],
+        seriesList: []
     },
     methods: {
         moviesGenerator: function() {
             const self = this;
             self.moviesList = [];
             axios
-                .get('https://api.themoviedb.org/3/search/movie?api_key=95d6ec199940a49c89273e717c71bedf&query=' + self.searchedMovie)
+                .get('https://api.themoviedb.org/3/search/movie?api_key=95d6ec199940a49c89273e717c71bedf&query=' + self.searched)
                 .then(function(the) {
                     for (let x = 0; x < the.data.results.length; x++) {
                         self.moviesList.push(the.data.results[x])
                     }
                 })
-            console.log(self.moviesList);
+        },
+        seriesGenerator: function() {
+            const self = this;
+            self.seriesList = [];
+            axios
+                .get('https://api.themoviedb.org/3/search/tv?api_key=95d6ec199940a49c89273e717c71bedf&query=' + self.searched)
+                .then(function(the) {
+                    for (let x = 0; x < the.data.results.length; x++) {
+                        self.seriesList.push(the.data.results[x])
+                    }
+                })
+        },
+        vote: function(movie, num1, num2) {
+            return {'fa' : true, 'fa-star': (Math.ceil(movie.vote_average / 2)) > num1, 'fa-star-o': (Math.ceil(movie.vote_average / 2)) <= num2}
         }
     }
 });
 Vue.config.devtools = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
