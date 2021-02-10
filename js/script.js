38558fd2055994baa16b2d9fe590f0466dd8ef94
Teresa Spigoni <teresaspigoni@gmail.com>
2021-02-10 20:08:28 +0100

    lang flags and layout

--- js/script.js
+++ js/script.js
@@ -12,9 +12,7 @@ new Vue({
             axios
                 .get('https://api.themoviedb.org/3/search/movie?api_key=95d6ec199940a49c89273e717c71bedf&query=' + self.searched)
                 .then(function(the) {
-                    for (let x = 0; x < the.data.results.length; x++) {
-                        self.moviesList.push(the.data.results[x])
-                    }
+                    self.moviesList = the.data.results;
                 })
         },
         seriesGenerator: function() {
@@ -23,13 +21,11 @@ new Vue({
             axios
                 .get('https://api.themoviedb.org/3/search/tv?api_key=95d6ec199940a49c89273e717c71bedf&query=' + self.searched)
                 .then(function(the) {
-                    for (let x = 0; x < the.data.results.length; x++) {
-                        self.seriesList.push(the.data.results[x])
-                    }
+                    self.seriesList = the.data.results;
                 })
         },
-        vote: function(movie, num1, num2) {
-            return {'fa' : true, 'fa-star': (Math.ceil(movie.vote_average / 2)) > num1, 'fa-star-o': (Math.ceil(movie.vote_average / 2)) <= num2}
+        vote: function(movie) {
+            return (Math.ceil(movie.vote_average / 2))
         }
     }
 });
